3edfe67a680f9969e855427e022b245501caf493
Trubarova <58742586+Trubarova@users.noreply.github.com>
2020-02-05 14:31:57 +0200

    Update and rename Task2 to Task_2.js

--- Task_2.js
+++ Task_2.js
@@ -1,11 +1,11 @@
-Lection1
+//Lection1
 let field = [-20,-15,-10,-3,5,18,26,38,44];
 const result = field.find(field => number.length = 2);
 console.log(result);
 
 
 
-Lection2
+//Lection2
 let A =[12,5,74,36,6,82,9,23];
 let B = [74,9,2,12,32,6,14,8];
 let result = true
