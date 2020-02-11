fecde85ffa752d03d416811efbcbc04eddc9ff4f
Trubarova <58742586+Trubarova@users.noreply.github.com>
2020-02-11 13:57:10 +0200

    Update Task_2.js

--- Task_2.js
+++ Task_2.js
@@ -6,7 +6,8 @@
 
 //Lection1
 let field = [-20,-15,-10,-3,5,18,26,38,44];
-const result = field.find(field => number.length = 2);
+const result = field.find(number =>  { 
+ if(field [i] > - 99 && field[i] <= -10) {}
 console.log(result);
 
 
@@ -14,12 +15,13 @@ console.log(result);
 //Lection2
 let A =[12,5,74,36,6,82,9,23];
 let B = [74,9,2,12,32,6,14,8];
-let result = true
+let result =[]
 for(var i = 0; i < A.length; i++);{
 for(var j = 0; j < B.length; J++);{
  if ( A[i] == B[j]){
-   continue;}
- } else;
-   break;
- console.log(result);
+  result[result.length] = A[i];
+ } 
+}
 };
+ console.log(result);
+
